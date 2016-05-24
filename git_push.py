70ce38c9311d2bddda8c6b969800adb64b524be7
sindyoung <deyoung_shen@msn.cn>
2016-05-24 09:49:11 +0800

    update git_push.py

--- git_push.py
+++ git_push.py
@@ -2,9 +2,27 @@
 
 import pexpect
 import os
+import sys
+
+def usage():
+	'''
+	1. export github_password='your_github_password'
+	2. ./git_push.py
+	'''
+	print('''
+	Usage:
+	  ./git_push.py
+	Note:
+	  you should set your github password in current shell
+	  e.g. export github_password='your_github_password'
+	''')
 
 def push():
-	password = os.environ['github_password']
+	password = os.getenv('github_password')
+	if password == None:
+		print('your github password parameter is not set yet')
+		usage()
+		sys.exit(1)
 	cmd = 'git push'
 	git = pexpect.spawn(cmd)
 	while True:
